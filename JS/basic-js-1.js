0285842f512da151cf205474f3ab8c9e794e7e74
ijpatter1 <ian@tunameltsmyheart.com>
2020-04-28 14:06:11 -0400

    Push files online
    Push files online for assignment

---
+++ JS/basic-js-1.js
@@ -0,0 +1,6 @@
+var A = "\"This is a string.\"";
+window.alert(A);
+var B = "Knock, knock." + " Who\'s there?";
+var C = "Chocolate", D = "Donut", E = "Eclair";
+var C = C.fontcolor("brown"), D = D.fontcolor("yellow"), E = E.fontcolor("green");
+document.write(C + D + E);
