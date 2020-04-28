20cfe9026c043e14755e57a9ded9738644f30d88
ijpatter1 <ian@tunameltsmyheart.com>
2020-04-28 14:47:59 -0400

    Push for assignment
    Push for assignment p.63

---
+++ JS/basic-js-2.js
@@ -0,0 +1,9 @@
+function knockKnock() {
+    var first = "Who\’s there?", second = "\nCandice.", third = "\nCandice who?", punchline="\nCandice joke get any worse?!";
+    document.getElementById("joke").innerHTML = first + second + third + punchline;
+}
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
